3cfc1b3f6fe31f9e2c176114e8ac7a77b5a47e3d
Thauan Amorim <thauanssussu@gmail.com>
2021-06-12 16:02:58 -0300

    adicao das msg de erro nos response do login

--- src/main/java/com/leilao/leilaoSite/application/adesao/controller/LoginController.java
+++ src/main/java/com/leilao/leilaoSite/application/adesao/controller/LoginController.java
@@ -6,8 +6,10 @@ import java.security.NoSuchAlgorithmException;
 import com.leilao.leilaoSite.application.adesao.service.LoginServiceImpl;
 import com.leilao.leilaoSite.domain.adesao.model.UserModel;
 import com.leilao.leilaoSite.presentation.authentication.dto.LoginDTO;
+import com.leilao.leilaoSite.presentation.authentication.exception.CredenciaisInvalidasException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,11 @@ public class LoginController {
     private LoginServiceImpl loginServiceImpl;
 
     @PostMapping
-    public UserModel login(@RequestBody LoginDTO loginDTO) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        return loginServiceImpl.fazerLogin(loginDTO);
+    public UserModel login(@RequestBody LoginDTO loginDTO) {
+        try {
+            return loginServiceImpl.fazerLogin(loginDTO);
+        } catch (CredenciaisInvalidasException e) {
+            throw e;
+        }
     }
 }
--- src/main/java/com/leilao/leilaoSite/application/adesao/service/LoginServiceImpl.java
+++ src/main/java/com/leilao/leilaoSite/application/adesao/service/LoginServiceImpl.java
@@ -7,6 +7,7 @@ import com.leilao.leilaoSite.domain.adesao.model.UserModel;
 import com.leilao.leilaoSite.domain.adesao.service.LoginService;
 import com.leilao.leilaoSite.infrastructure.persistence.repository.user.UserRepository;
 import com.leilao.leilaoSite.presentation.authentication.dto.LoginDTO;
+import com.leilao.leilaoSite.presentation.authentication.exception.CredenciaisInvalidasException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,20 +21,21 @@ public class LoginServiceImpl implements LoginService{
     private UserServiceImpl userServiceImpl;
 
     @Override
-    public UserModel fazerLogin(LoginDTO loginDTO) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public UserModel fazerLogin(LoginDTO loginDTO) {
         UserModel user = userRepository.findByUsername(loginDTO.getUsername());
 
         boolean status = false;
         if(user != null) {
-            status = userServiceImpl.verificarSenha(loginDTO.getSenha(), user.getPassword());
+            try {
+                status = userServiceImpl.verificarSenha(loginDTO.getSenha(), user.getPassword());
+            } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+                throw new CredenciaisInvalidasException();
+            }
         }
-
-        System.out.println(status);
 
         if(status) {
             return user;
         }
-        return null;
+        throw new CredenciaisInvalidasException();
     }
-    
 }
