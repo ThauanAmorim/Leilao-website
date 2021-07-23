6d44f3fb8abc4c454e7310a3a6ae82c056231cb9
Thauan Amorim <thauanssussu@gmail.com>
2021-07-22 22:55:47 -0300

    resolucao do cors

--- backend/src/main/java/com/leilao/leilaoSite/application/adesao/controller/LeilaoController.java
+++ backend/src/main/java/com/leilao/leilaoSite/application/adesao/controller/LeilaoController.java
@@ -14,16 +14,19 @@ import com.leilao.leilaoSite.presentation.leilao.LeilaoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/leilao")
+@CrossOrigin
 public class LeilaoController {
     @Autowired
     private LeilaoService leilaoService;
--- backend/src/main/java/com/leilao/leilaoSite/infrastructure/security/auth/SecurityConfiguration.java
+++ backend/src/main/java/com/leilao/leilaoSite/infrastructure/security/auth/SecurityConfiguration.java
@@ -14,9 +14,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     
     @Autowired
     private UserServiceSecurity userService;
@@ -31,7 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/api/user", "/api/login").permitAll().anyRequest()
+        http.cors().and().csrf().disable().authorizeRequests().antMatchers("/api/user", "/api/login").permitAll().anyRequest()
             .authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         
         http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
@@ -47,5 +49,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
 }
