3e5ac5e646710894067006fdba506ccd49d2f497
Thauan Amorim <thauanssussu@gmail.com>
2021-07-26 00:51:24 -0300

    adicao de alguns listeners para a pagina principal

--- frontend/src/typescript/PaginaPrincipal.ts
+++ frontend/src/typescript/PaginaPrincipal.ts
@@ -10,16 +10,57 @@ async function irPaginaLogin () {
 }
 
 async function listarprodutos () {
-  let token = "Bearer " + window.localStorage.getItem("token");
-  // console.log(token);
   const rawResponse = await fetch('http://localhost:8080/api/leilao', {
     method: 'GET',
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': token
+      'Content-Type': 'application/json'
     }
   });
-  rawResponse.json().then(t => console.log(t[0]));
+  renderizarProdutos(rawResponse.json());
+}
+
+function addListener(lista : NodeListOf<Element>) {
+  lista.forEach(elementLista => {
+    let element = elementLista as HTMLElement;
+    element.addEventListener("mousemove", () => {
+      element.style.borderStyle = "solid";
+      element.style.borderColor = "#E7E7E7"
+    });
+
+    element.addEventListener("mouseleave", () => {
+      element.style.border = null;
+    });
+
+    element.addEventListener("click", () => {
+      window.localStorage.setItem("produto", element.getElementsByClassName("id").item(0).textContent);
+      window.location.href = "../pages/telaLeilao.html";
+    });
+  });
+}
+
+function renderizarProdutos(json : Promise<any>) {
+  let catalogo = document.getElementById("catalogo")
+  json.then(dados => {
+    let i = 0;
+    let html = "";
+    while(true) {
+      if(dados[i] === undefined) {
+        break;
+      }
+      let elemento = dados[i++];
+      console.log(elemento);
+      html += `<div class="item_catalogo" id="item_catalogo">
+      <div id="imagens" class="imagens"></div>
+      <label id="id" class="id" for="">${elemento["id"]}</label>
+      <label id="data_inicio" for="">${elemento["dataInicio"]}</label>
+      <label id="valor_atual" for="">${elemento["valorAtual"]}</label>
+      <label id="valor_inicial" for="">${elemento["valorInicial"]}</label>
+      <label id="nome_ultimo_lance" for="">${elemento["usernameUltimoLance"]}</label>
+      </div>`;
+    }
+    catalogo.innerHTML = html;
+    addListener(catalogo.querySelectorAll('[id=item_catalogo]'));
+  })
 }
 
 listarprodutos();
